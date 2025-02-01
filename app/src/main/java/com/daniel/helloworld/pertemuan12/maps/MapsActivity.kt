8f372b24c4cfc6fd8ebc771e24c5b16a69b916af
Aquaries <danielaquaries@gmail.com>
2025-02-01 10:52:11 +0700

    Kelas Industri Middle - Google Maps Geofence Maps - Map Are

--- app/src/main/java/com/daniel/helloworld/pertemuan12/maps/MapsActivity.kt
+++ app/src/main/java/com/daniel/helloworld/pertemuan12/maps/MapsActivity.kt
@@ -1,13 +1,20 @@
 package com.daniel.helloworld.pertemuan12.maps
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.crocodic.core.base.activity.NoViewModelActivity
 import com.crocodic.core.extension.checkLocationPermission
+import com.crocodic.core.helper.LocationHelper
 import com.daniel.helloworld.R
 import com.daniel.helloworld.databinding.ActivityTrialMapBinding
 import com.daniel.helloworld.helper.PenolongLokasi
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -16,7 +23,7 @@ import javax.inject.Inject
 class MapsActivity : NoViewModelActivity<ActivityTrialMapBinding>(R.layout.activity_trial_map) {
 
     @Inject
-    lateinit var adrHelper : PenolongLokasi
+    lateinit var adrHelper: PenolongLokasi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +33,8 @@ class MapsActivity : NoViewModelActivity<ActivityTrialMapBinding>(R.layout.activ
         checkLocationPermission {
             listenLocationChange()
         }
-        binding.mapView.getMapAsync {googleMap ->
-            googleMap.setOnCameraMoveListener {
+        binding.mapView.getMapAsync { googleMap ->
+            /*googleMap.setOnCameraMoveListener {
                 binding.ivTarget.alpha = 0.5f
             }
 
@@ -40,14 +47,44 @@ class MapsActivity : NoViewModelActivity<ActivityTrialMapBinding>(R.layout.activ
                 adrHelper.getAddress(LatLng(curLocation.latitude, curLocation.longitude)) {
                     binding.tvAddress.text = it
                 }
+            }*/
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return@getMapAsync
             }
+                googleMap.isMyLocationEnabled = true
 
+            val area = googleMap.addCircle(CircleOptions()
+                .center(LatLng(-7.0644051, 110.4139525))
+                .radius(1_000.0)
+                .strokeColor(Color.parseColor("#FFC80000"))
+                .fillColor(Color.parseColor("#25C80000")))
         }
+    }
+
+    private fun isInsideLocation(area: LatLng, position: LatLng) : Boolean {
+        return LocationHelper.distance(area, position) < 1
     }
 
     override fun retrieveLocationChange(location: Location) {
         super.retrieveLocationChange(location)
         Log.d("deviceLocation", "latitude: ${location.latitude}, longitude: ${location.longitude}")
+
+        binding.mapView.getMapAsync {
+            it.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 13f))
+
+            val isInside = isInsideLocation(LatLng(-7.0644051, 110.4139525), LatLng(location.latitude, location.longitude))
+
+            val status = if(isInside) "dalam" else "luar"
+
+            binding.tvStatus.text = "Kamu berada di $status area."
+        }
     }
 
     override fun onStart() {
